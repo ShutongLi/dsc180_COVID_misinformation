f0e0f0b0cdc8186bb03fd1958558f43e2f34a06a
hhliou <hhliou@hhliou-1641.pods.hhliou.svc.cluster.local>
2020-10-27 14:29:54 -0700

    Config, data pipeline, run updated

--- run.py
+++ run.py
@@ -1,11 +1,28 @@
 import sys
 import json
+from src.data import generate_dataset
 
 
 # main operation
 def main():
-    return
-
+    # Load configurations
+    data_params = json.loads('/dsc180_project_structure/config/data_params.json')
+    sample_params = json.loads('/dsc180_project_structure/config/sample_params.json')
+    
+    # Cfg variables
+    raw_data_path = data_params['raw_data_path']
+    dehydrated_data_path = data_params['dehydrated_data_path']
+    rehydrated_data_path = data_params['rehydrated_data_path']
+    id_column = data_params['id_column']
+    
+    seed = sample_params['seed']
+    sample_size = sample_params['sample_size']
+    
+    # Clean raw data
+    generate_dataset.rehydrate_tweets(raw_data_path, dehydrated_data_path, hydrated_data_path, seed, sample_size, id_column)
+    
+    # Load cleaned data
+    tweets = read_dataframe(hydrated_data_path)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
---
+++ src/data/generate_dataset.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import json
+
+def rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column)
+    # Sample data to rehydrate
+    df = pd.read_table(raw_data_path)
+    id_sample = df[id_column].sample(n=sample_size, replace = False, random_state = seed)
+
+    # Create text file for rehydration
+    id_sample.to_csv(dehydrated_data_path, index=False, header=False)
+
+    # Rehydrate text file
+    os.system('twarc hydrate ' + dehydrated_data_path + ' > ' + rehydrated_data_path)
+    
+def read_dataframe(rehydrated_data_path):
+    return pd.read_csv(rehydrated_data_path)
