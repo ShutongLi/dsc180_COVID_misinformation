ae1e6eebb58a7e80212cef41ccaa2c2859d64a6b
shl636 <shl636@shl636-9012.pods.shl636.svc.cluster.local>
2020-10-27 16:14:24 -0700

    data ingestion

--- run.py
+++ run.py
@@ -16,16 +16,19 @@ def main():
     dehydrated_data_path = data_params['dehydrated_data_path']
     rehydrated_data_path = data_params['rehydrated_data_path']
     twarc_path = data_params['twarc_path']
+    rehydrated_df_path = data_params['rehydrated_df_path']
     id_column = data_params['id_column']
     
     seed = sample_params['seed']
     sample_size = sample_params['sample_size']
     
     # sample raw data of tweet ids, rehydrate them (enrich them with tweet contents)
-    generate_dataset.rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column, twarc_path)
+#     generate_dataset.rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column, twarc_path)
     
     # Load cleaned data
-    tweets = generate_dataset.read_dataframe(rehydrated_data_path)
+    tweets = generate_dataset.generate_dataframe(rehydrated_data_path, rehydrated_df_path)
+    
+#     return display_dataframe()
 
 if __name__ == '__main__':
     args = sys.argv[1:]
--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -14,12 +14,21 @@ def rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, 
 #     os.system('twarc configure')
     # Rehydrate text file
     os.system(f'{twarc_location} hydrate {dehydrated_data_path} > {rehydrated_data_path}')
+
+
+# !!! file path for the jsonl is changed to relative here due to unknown error
+def generate_dataframe(rehydrated_data_path, rehydrated_df_path):
+    rehydrated_fp = rehydrated_data_path.replace('~/dsc180_project_structure/', '')
+    rehydrated_df_fp = rehydrated_df_path
     
-def read_dataframe(rehydrated_data_path):
-    rehydrated_fp = rehydrated_data_path
     with open(rehydrated_fp) as rh_fh:
         json_content = rh_fh.read()
     rows = []
     for line in json_content.splitlines():
         rows.append(json.loads(line))
-    return pd.read_csv(rehydrated_data_path)
+    tweets = pd.DataFrame(rows)
+    tweets['hashtags'] = tweets['entities'].apply(lambda x: [tag['text'] for tag in x['hashtags']])
+    tweets.to_csv(rehydrated_df_fp, index = False)
+    
+def display_dataframe(rehydrated_df_path):
+    return pd.read_csv(rehydrated_df_path)
