95950157a24ef1a937446bba08de4dc456b2b1dd
hhliou <hhliou@hhliou-1641.pods.hhliou.svc.cluster.local>
2020-10-27 14:51:48 -0700

    Minor file fixes

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ from src.data import generate_dataset
 # main operation
 def main():
     # Load configurations
-    data_params = json.loads('/dsc180_project_structure/config/data_params.json')
-    sample_params = json.loads('/dsc180_project_structure/config/sample_params.json')
+    with open('./config/data_params.json') as f:
+        data_params = json.load(f)
+    with open('./config/sample_params.json') as f:
+        sample_params = json.load(f)
     
     # Cfg variables
     raw_data_path = data_params['raw_data_path']
@@ -19,10 +21,10 @@ def main():
     sample_size = sample_params['sample_size']
     
     # Clean raw data
-    generate_dataset.rehydrate_tweets(raw_data_path, dehydrated_data_path, hydrated_data_path, seed, sample_size, id_column)
+    generate_dataset.rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column)
     
     # Load cleaned data
-    tweets = read_dataframe(hydrated_data_path)
+    tweets = read_dataframe(rehydrated_data_path)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import json
 
-def rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column)
+def rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column):
     # Sample data to rehydrate
     df = pd.read_table(raw_data_path)
     id_sample = df[id_column].sample(n=sample_size, replace = False, random_state = seed)
@@ -10,6 +10,8 @@ def rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, 
     # Create text file for rehydration
     id_sample.to_csv(dehydrated_data_path, index=False, header=False)
 
+    # Input API keys
+    os.system('twarc configure')
     # Rehydrate text file
     os.system('twarc hydrate ' + dehydrated_data_path + ' > ' + rehydrated_data_path)
     
