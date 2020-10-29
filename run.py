5fd8e0960aaa5b4264d45bd0250ec286743fca9e
hhliou <hhliou@hhliou-21537.pods.hhliou.svc.cluster.local>
2020-10-29 00:19:25 -0700

    Implemented data download by date

--- run.py
+++ run.py
@@ -13,22 +13,24 @@ def main():
     
     # Cfg variables
     raw_data_path = data_params['raw_data_path']
-    dehydrated_data_path = data_params['dehydrated_data_path']
-    rehydrated_data_path = data_params['rehydrated_data_path']
+    processed_data_path = data_params['dehydrated_data_path']
     twarc_path = data_params['twarc_path']
     rehydrated_df_path = data_params['rehydrated_df_path']
     id_column = data_params['id_column']
+    day = data_params['day']
+
+    seed = sample_params['seed']
+    sample_size = sample_params['sample_size']
     
     seed = sample_params['seed']
     sample_size = sample_params['sample_size']
     
+    
+    
+    # Update dataset from some date (set in data_params) to today
+    generate_dataset.download_latest_datasets(raw_data_path, day)
     # sample raw data of tweet ids, rehydrate them (enrich them with tweet contents)
-    generate_dataset.rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column, twarc_path)
-    
-    # Load cleaned data
-    tweets = generate_dataset.generate_dataframe(rehydrated_data_path, rehydrated_df_path)
-    
-    return generate_dataset.display_dataframe(rehydrated_df_path)
+    print(generate_dataset.rehydrate_tweets(raw_data_path, processed_data_path, seed, sample_size, id_column, twarc_path, day))
 
 if __name__ == '__main__':
     args = sys.argv[1:]
--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -1,34 +1,76 @@
 import os
 import pandas as pd
-import json
+import requests
+import gzip
+import shutil
+from datetime import datetime, date, timedelta
 
-def rehydrate_tweets(raw_data_path, dehydrated_data_path, rehydrated_data_path, seed, sample_size, id_column, twarc_location):
+# Get the last_day_added based on the latest date in your dataset
+def get_last_day(raw_data_path, from_time):
+    # Failsafe in case you have no raw data
+    last_day_added = datetime.strptime(from_time, '%Y-%m-%d').date()
+    # Get all filenames of our raw data
+    for name in os.listdir(raw_data_path):
+        # Just in case a file name doesn't have the date
+        try:
+            # Get the date from the name
+            name_date = datetime.strptime(name[:10], '%Y-%m-%d').date()
+            # Get maximum date
+            if last_day_added < name_date:
+                last_day_added = name_date
+        except:
+            print(f'{name} does not have a date.')
+    return last_day_added
+
+# Updates your dataset FROM from_time date (YYYY-MM-DD) TO today
+def download_latest_datasets(raw_data_path, from_time):
+    # Get days of data between range
+    date_today = date.today()
+    last_day = get_last_day(raw_data_path, from_time)
+    days_between = pd.date_range(last_day, date_today, freq='d')
+    
+    # Download the data from each day
+    for day in days_between:
+        
+        # Download gzipped file
+        day_str = datetime.strftime(day, '%Y-%m-%d')
+        url = f'https://github.com/thepanacealab/covid19_twitter/blob/master/dailies/{day_str}/{day_str}_clean-dataset.tsv.gz?raw=true'
+        
+        try:
+            # Make directories if they don't already exist
+            os.mkdir(f'{raw_data_path}/gzips')
+        except:
+            pass
+        
+        # Separate gzipped and TSV files
+        try:
+            filename = f'{raw_data_path}/gzips/{day_str}_clean-dataset.tsv.gz'
+            filename2 = f'{raw_data_path}/{day_str}_clean-dataset.tsv'
+            with open(filename, 'wb') as f:
+                r = requests.get(url)
+                f.write(r.content)
+
+            # Download TSV
+            with gzip.open(filename, 'rb') as f_in:
+                with open(filename2, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+        except:
+            print(f'There is no data for {day_str}')
+            os.remove(filename)
+            os.remove(filename2)
+    return 
+
+# Get the information from raw tweets we just obtained
+def rehydrate_tweets(raw_data_path, processed_data_path, seed, sample_size, id_column, twarc_location, day):
     # Sample data to rehydrate
-    df = pd.read_table(raw_data_path)
+    df = pd.read_table(f'{raw_data_path}/{day}_clean-dataset.tsv')
     id_sample = df[id_column].sample(n=sample_size, replace = False, random_state = seed)
 
     # Create text file for rehydration
-    id_sample.to_csv(dehydrated_data_path, index=False, header=False)
+    id_sample.to_csv(f'{processed_data_path}/{day}_tweet_ids', index=False, header=False)
 
-    # Input API keys
-#     os.system('twarc configure')
     # Rehydrate text file
-    os.system(f'{twarc_location} hydrate {dehydrated_data_path} > {rehydrated_data_path}')
-
-
-# !!! file path for the jsonl is changed to relative here due to unknown error
-def generate_dataframe(rehydrated_data_path, rehydrated_df_path):
-    rehydrated_fp = rehydrated_data_path.replace('~/dsc180_project_structure/', '')
-    rehydrated_df_fp = rehydrated_df_path
+    os.system(f'{twarc_location} hydrate {processed_data_path}/{day}_tweet_ids > {processed_data_path}/{day}_tweets_hydrated.jsonl')
     
-    with open(rehydrated_fp) as rh_fh:
-        json_content = rh_fh.read()
-    rows = []
-    for line in json_content.splitlines():
-        rows.append(json.loads(line))
-    tweets = pd.DataFrame(rows)
-    tweets['hashtags'] = tweets['entities'].apply(lambda x: [tag['text'] for tag in x['hashtags']])
-    tweets.to_csv(rehydrated_df_fp, index = False)
-    
-def display_dataframe(rehydrated_df_path):
-    return pd.read_csv(rehydrated_df_path)
+    # Display example
+    return pd.read_json(f'{processed_data_path}/{day}_tweets_hydrated.jsonl', lines=True).head()
