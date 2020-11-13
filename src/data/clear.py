21565a827b5aab8597a939c00a99d30732edb183
hhliou <hhliou@hhliou-31949.pods.hhliou.svc.cluster.local>
2020-11-13 06:24:25 -0800

    Data Collection/Rehydration Overhauled, run.py updated

--- src/data/clear.py
+++ src/data/clear.py
@@ -1,24 +1,26 @@
-#CLEAR ANY DATA THAT HAS DOWNLOADED SO FAR BANGBANGBAGN
+# Clear any data that has been downloaded
 
 import os
 import shutil
 
-# helper function
+# Delete all files and directories in a path
 def obliviate(path):
     for fname in os.listdir(path):
         print(f'deleting{fname} under {path}')
         file_path = os.path.join(path, fname)
         try:
+            # Delete file in path
             if os.path.isfile(file_path):
-                # ELDRITCH BLAST!!!!
                 os.remove(file_path)
+            
+            # Delete directory in path
             elif os.path.isdir(file_path):
-                # ELDRITCH BLAST!!!!
                 shutil.rmtree(file_path)
         except Exception as e:
+            # Failsafe: potential error for deleting paths
             print(f'Failed to delete {file_path}. Reason: {e}')
 
-# clear script
+# Delete files for all paths
 def clean(paths):
     for path in paths:
         obliviate(path)
