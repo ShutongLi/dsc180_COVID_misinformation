eaab6466f46f904a250b54073b68af0065c6a5a5
shl636 <shl636@shl636-15318.pods.shl636.svc.cluster.local>
2020-12-09 05:11:48 -0800

    step rework and step 3

--- src/features/generate_features.py
+++ src/features/generate_features.py
@@ -1,12 +1,22 @@
 import os
 import pandas as pd
 import numpy as np
+import tweepy
+import json
+import datetime
 
 # Count the number of occurences of every hashtag in the JSON
-def hashtag_counts(json):
+def hashtag_counts(json, marker_ht = None):
     df = pd.read_json(json, lines = True)
     ht = df['entities'].apply(lambda e: [x['text'] for x in e['hashtags']])
-    return pd.Series(ht.sum()).value_counts()
+    ht_vc = pd.Series(ht.sum()).value_counts()
+    if marker_ht is not None:
+        num_tweets = len(df)
+        subset_ht = ht.apply(lambda x: x if set(x) & set(marker_ht) else None).dropna()
+        marker_num_tweets = len(subset_ht)
+        marker_vc = pd.Series(subset_ht.sum()).value_counts()
+        return ht_vc, marker_vc, num_tweets, marker_num_tweets
+    return ht_vc
 
 
 # Count the number of posts every user has made in the JSON
@@ -17,13 +27,33 @@ def user_counts(json):
 
 
 # Count either hashtags or users in all available JSON files
-def count_features(jsons, top_k = None, mode = 'hashtag'):
+def count_features(jsons, top_k = None, mode = 'hashtag', marker_ht = None):
     # Decide whether to count hashtags or users
     if mode == 'hashtag':
         method = hashtag_counts
     elif mode == 'user':
         method = user_counts
-        
+    
+    ### POLARITY: having marker_ht indicates that we are calculating polarity
+    if marker_ht is not None:
+        print('polarity mode')
+        total_series, marker_series, num_tweets, marker_num_tweets = method(jsons[0], marker_ht = marker_ht)
+        print(f'vc shape {total_series.shape}', end='\r')
+    
+        # Append counts to every subsequent JSON, top_k has to be defined
+        for json in jsons[1:]:
+            vc, marker_vc, new_num_tweets, new_marker_num_tweets = method(json, marker_ht = marker_ht)
+            total_series = total_series.add(vc, fill_value = 0)
+            marker_series = marker_series.add(marker_vc, fill_value = 0)
+            num_tweets += new_num_tweets
+            marker_num_tweets += new_marker_num_tweets
+            print(f'vc shape {total_series.shape}', end='\r')   
+        # make sure top-k is defined
+        topk_vc = total_series.sort_values(ascending = False).iloc[:top_k]
+        # note that marker_series is the entire series, rather than top-K
+        return topk_vc, marker_series, num_tweets, marker_num_tweets
+    
+    ### Visualization: we are only interested in number of hashtags
     # Compile count of first JSON in list
     total_series = method(jsons[0])
     print(f'vc shape {total_series.shape}', end='\r')
@@ -35,7 +65,11 @@ def count_features(jsons, top_k = None, mode = 'hashtag'):
         print(f'vc shape {total_series.shape}', end='\r')
         
     # Return the top users/hashtags in all of the data
-    return total_series.sort_values().sort_values(ascending=False)
+    result = total_series.sort_values(ascending = False)
+    
+    if top_k is not None:
+        return result.iloc[:top_k]
+    return result
 
 
 # Get the number of certain used hashtags over time
@@ -47,3 +81,99 @@ def count_over_time(jsons, good_tags, bad_tags):
         good.append(vc.reindex(good_tags).fillna(0))
         bad.append(vc.reindex(bad_tags).fillna(0))
     return good, bad
+
+### BELOW IS FOR CALCULATING HASHTAG POLARITY
+
+# wrapper for count_features to calculate baseline rate of occurence of top 200 hashtags
+def hashtag_polarity(jsons, top_k, marker_ht):
+    top200_vc, marker_vc, total_num_tweets, marker_num_tweets =  count_features(jsons, top_k, marker_ht = marker_ht)
+    print(f'there are {total_num_tweets} tweets and {marker_num_tweets} subset tweets')
+    baseline_ROF = top200_vc / total_num_tweets
+    # marker_vc contains all hashtags in the marker 
+    # marker_ROF is the ROF of 200 under the subsetted marker value_counts
+    marker_ROF = marker_vc.reindex(top200_vc.index, fill_value=0) / marker_num_tweets
+    return (marker_ROF - baseline_ROF) / baseline_ROF
+
+
+### BELOW IS FOR CALCULATING HASHTAG POLARITY
+# find all user names
+def collect_all_users(jsons):
+    all_users = set()
+    for json in jsons:
+        df = pd.read_json(json, lines=True)
+        us = set(df['user'].apply(lambda x: x['screen_name']))
+        all_users = all_users.union(us)
+        print(f'{len(all_users)} users in total', end='\r')
+    return pd.Series(list(all_users))
+
+# find the all the tweets of a user between dates
+def search_tweets(api, username, t200_ht, lower, upper, date_pattern, max_posts = None, max_iter = None):
+    assert type(t200_ht) == set, "make sure pass in a SET of hashtags"
+    upper = pd.to_datetime(upper)
+    lower = pd.to_datetime(lower)
+    # all the hashtags ever used by a user
+    hashtags = []
+    i = 0
+    tweet_stored = 0
+    # number of tweets that contains t200 hashtags
+    num_norm_tweets = 0
+    for status in tweepy.Cursor(api.user_timeline, screen_name=username).items():
+        if ((max_posts is not None) and (tweet_stored > max_posts)) \
+        or ((max_iter is not None) and (i > max_iter)):
+            break
+        # process status here
+        ajson = status._json
+        created_at = ajson['created_at']
+        print(i, f'{tweet_stored} tweets qualified', created_at, end = '\r')
+        creation_date = datetime.datetime.strptime(created_at, date_pattern).replace(tzinfo = None)
+        if lower < creation_date < upper:
+            hashtags_of_tweet = [x['text'] for x in ajson['entities']['hashtags']]
+            if set(hashtags_of_tweet) &  t200_ht:
+                num_norm_tweets += 1
+            hashtags.extend(hashtags_of_tweet)
+            tweet_stored += 1
+        i += 1
+    # return value count of hashtags
+    return pd.value_counts(pd.Series(hashtags)), num_norm_tweets
+
+# return normalized user polarity
+def user_polarity(api, username, t200_ht, lower, upper, 
+                  date_pattern, 
+                  ht_polarity, 
+                  max_posts = None, max_iter = None,
+                  normalize = True):
+    print(f'investigating {username}')
+    user_ht_counts, num_norm_tweets = search_tweets(api, username, t200_ht, 
+                                                    lower, upper, date_pattern, 
+                                                    max_posts = max_posts, max_iter = max_iter)
+    toi = user_ht_counts.reindex(ht_polarity.index, fill_value=0)
+    u_pol = (toi * ht_polarity).sum()
+    if normalize:
+        if num_norm_tweets == 0:
+            return None
+        u_pol /= num_norm_tweets
+    return u_pol
+
+### Investigate retweets of a tweet
+def investigate_retweets(api, tid, num_retrieve_con, t200_ht, lower, upper, 
+                         date_pattern, 
+                         ht_polarity, 
+                         max_posts = None, max_iter = None,
+                         normalize = True):
+    retweets = api.retweets(tid, num_retrieve_con)
+    names = [r.user.screen_name for r in retweets]
+    fh = open('con.json', "r+")
+    user_polarity_dict = json.load(fh)
+    for name in names:
+        if name not in user_polarity_dict.keys():
+            score = user_polarity(api, name,
+                                  set(ht_polarity.index), 
+                                  lower, upper, date_pattern,
+                                  ht_polarity, 
+                                  max_posts = max_posts, 
+                                  max_iter=max_iter,
+                                 )
+            user_polarity_dict[name] = score
+            print(f'{name}: {score}')
+        json.dump(user_polarity_dict, fh)
+    return user_polarity_dict
